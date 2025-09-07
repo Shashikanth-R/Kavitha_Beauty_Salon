4c5b0703e8eb8f568521833f3d054a1895892f4a
Shashikanth-R <kanthrshashi@gmail.com>
2025-09-07 18:07:52 +0530

    Initial commit - Kavitha Beauty Salon

---
+++ config.py
@@ -0,0 +1,11 @@
+class Config:
+    SECRET_KEY = 'your_secret_key_here'
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'root'
+    MYSQL_PASSWORD = 'Shashi@30'
+    MYSQL_DB = 'beauty_salon'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USERNAME = 'shr23cs@cmrit.ac.in'
+    MAIL_PASSWORD = 'Shashi@3005'
+    MAIL_SENDER = 'shr23cs@cmrit.ac.in'
